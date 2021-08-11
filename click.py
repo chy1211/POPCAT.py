334e1a487de9953b7634a00e1a56acbae9dd4fd4
chy1211 <henry70304@gmail.com>
2021-08-11 20:09:06 +0800

    use tkinter

--- click.py
+++ click.py
@@ -1,13 +1,23 @@
 from selenium import webdriver
+import tkinter as tk
+from tkinter import Button
 import time
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(options=options)
-driver.get("https://popcat.click/")
+def start():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://popcat.click/")
+    image=driver.find_element_by_class_name("cat-img.p")
+    while True:
+        image.click()
+        time.sleep(0.038)
 
-image=driver.find_element_by_class_name("cat-img.p")
+win = tk.Tk()
 
-while True:
-    image.click()
-    time.sleep(0.038)
+win.title("POPCAT!!!!")
+
+btn_start = Button(text="開始", command=start)
+btn_start.grid(column=0, row=0)
+
+win.mainloop()
